7bcf23c38173de509f59192971d704323b0841cc
M4nuuuu <74206827+M4nuuuu@users.noreply.github.com>
2021-06-04 19:59:32 +0200

    Addes -music-animation-earthzone-newenemystructure

---
+++ images.g.ts
@@ -0,0 +1,72 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . f f f f f f . . . . . . . . 
+. . 6 6 6 6 6 . . f 8 . . . . . 
+. . 6 8 8 8 6 6 6 6 7 8 . . . . 
+4 5 4 4 4 5 8 7 7 7 f 7 8 . . . 
+5 4 5 5 4 4 8 7 7 7 f 7 8 . . . 
+. . 6 8 8 8 6 6 6 6 7 8 . . . . 
+. . 6 6 6 6 6 . . f 8 . . . . . 
+. . f f f f f f . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":return img`
+. . f f f . . . . . . . . . . . 
+f f f c c . . . . . . . . f f f 
+f f c c c . c c . . . f c b b c 
+f f c 3 c c 3 c c f f b b a c . 
+f f c 3 b c 3 b c f b a a c c . 
+f c b b b b b b c f a c b c c . 
+c c 1 1 b 1 1 b c b b c b b c . 
+c b b b b b b b b b c c c b c . 
+c b 1 f f 1 c b b c c 2 c c . . 
+c f 1 f f 1 f b b b 2 2 c . . . 
+f f f f f f f b b a b f c . . . 
+f f 2 2 2 2 f b a a b f c c . . 
+. f 2 2 2 2 2 b a b c f . . . . 
+. . f 2 2 2 b b b c f . . . . . 
+. . . f f f f 2 2 2 . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 8 2 2 2 2 . . . . . . 
+. . . 8 8 5 4 5 4 5 2 . . . . . 
+. . . . . 8 2 2 2 2 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
